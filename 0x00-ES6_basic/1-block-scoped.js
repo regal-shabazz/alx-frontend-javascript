679457ff57fbb6a7e04950d1739954b1b5b3f60b
Oshin Babatunde <orsheenxi@gmail.com>
2024-03-06 01:38:25 +0000

    modified second task 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-     return [task, task2]
+    return [task, task2];
   }
 
   return [task, task2];
