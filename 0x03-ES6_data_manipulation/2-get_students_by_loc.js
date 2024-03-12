47b4d8df9a88d1dec3b56e68efe2573978b14438
Oshin Babatunde <orsheenxi@gmail.com>
2024-03-12 18:15:44 +0000

    task 2, filter block statement ammended

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  return students.filter((student) => {
-    return student.location === city;
-  });
+  return students.filter((student) => student.location === city);
 }
